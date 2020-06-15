b4930fbc42cf0e7200f075053fe97b09aa98d075
khanhmhq99 <qkhanhpro99@gmail.com>
2020-06-15 14:37:05 +0700

    code ne

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:gym_app/homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    home: GymApp(),
+      );
+  }
+}
+class GymApp extends StatefulWidget {
+  GymAppState createState() => GymAppState();
+}
+
+ 
+
+class GymAppState extends State {
+  final List<Widget> listWidget = [
+    HomePage(),
+    Container(),
+    Container(),
+    Container(),
+    Container(),
+
+  ];
+
+  int _currentIndex= 0;
+  void indexLast(int index){
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+      title: 'Flutter Demo',
+      home: Scaffold(
+        body: listWidget.elementAt(_currentIndex),
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _currentIndex,
+            onTap: indexLast,
+
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+              backgroundColor: Colors.cyan[900]
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(MaterialCommunityIcons.dumbbell),
+              title: Text('Trainning'),
+              backgroundColor: Colors.cyan[900]
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(AntDesign.shoppingcart),
+              title: Text('Cart'),
+              backgroundColor: Colors.cyan[900]
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Octicons.report),
+              title: Text('Report'),
+              backgroundColor: Colors.cyan[900]
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Entypo.menu),
+              title: Text('Me'),
+              backgroundColor: Colors.cyan[900]
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/sample.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ItemPage extends StatefulWidget {
+  @override
+  ItemPageState createState() => ItemPageState();
+}
+
+class ItemPageState extends State<ItemPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.red
+      ),
+    );
+  }
+}
